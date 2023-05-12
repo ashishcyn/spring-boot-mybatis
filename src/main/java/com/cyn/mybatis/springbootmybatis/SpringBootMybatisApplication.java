3a2b1eeb2d3d80f29cf3c4f74126e3d393ccf09e
ashishcyn <c2nashish@gmail.com>
2023-05-12 18:42:34 +0100

    CYN-005: MapperScan and MappedTypes configured

--- src/main/java/com/cyn/mybatis/springbootmybatis/SpringBootMybatisApplication.java
+++ src/main/java/com/cyn/mybatis/springbootmybatis/SpringBootMybatisApplication.java
@@ -1,8 +1,13 @@
 package com.cyn.mybatis.springbootmybatis;
 
+import com.cyn.mybatis.springbootmybatis.model.User;
+import org.apache.ibatis.type.MappedTypes;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.cyn.mybatis.springbootmybatis.mapper")
+@MappedTypes(User.class)
 @SpringBootApplication
 public class SpringBootMybatisApplication {
 
